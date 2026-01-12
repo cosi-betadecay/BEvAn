64e2fe49a39416958bd372168fd5a4756e6c5ec6
Arya <aryaraeesi@blinkweb.no>
2026-01-12 02:42:11 -0800

    Solution that works, not sure of the physics regarding sigma_cos_phi in the compton kinematic preprocessing step.

--- physics/preprocessing/compton_kin.py
+++ physics/preprocessing/compton_kin.py
@@ -10,9 +10,6 @@ def compton_kin(energies: torch.Tensor) -> bool:
     Returns:
         bool: True if the angle is physically valid, False otherwise.
     """
-    # Get's integrated to the beta decay function, that's why we return True for less than 2 hits.
-    if torch.Tensor(energies).numel() < 2:
-        return True
 
     def sigma_cos_phi(E_0: torch.Tensor, E: torch.Tensor, frac_sigma: float = 0.0035) -> float | None:
         """Calculate the uncertainty in the cosine of the Compton scattering angle.
@@ -35,17 +32,20 @@ def compton_kin(energies: torch.Tensor) -> bool:
 
         return electron_mass_energy * torch.sqrt(term0 + term1)
 
+    energies = energies[energies > 0]
+
+    if energies.numel() < 2:
+        return True
+
     E_0 = energies.sum()
     E = energies[1:].sum()
 
     electron_mass_energy = 511.0  # keV
     cos_phi = 1 - electron_mass_energy * (1 / E - 1 / E_0)
 
-    _sigma_cos_phi = sigma_cos_phi(E_0, E)
+    _sigma_cos_phi = 75 * sigma_cos_phi(E_0, E)
 
     limit = 1.0 + _sigma_cos_phi
-
-    # print(energies)
 
     return bool(torch.abs(cos_phi) <= limit)
 
@@ -53,14 +53,11 @@ def compton_kin(energies: torch.Tensor) -> bool:
 def compton_kin_preprocessing(
     energies: torch.Tensor, positions: torch.Tensor
 ) -> tuple[torch.Tensor, torch.Tensor]:
-    # Build validity mask per sequence
     valid_mask = torch.tensor(
         [compton_kin(energies[i]) for i in range(energies.shape[0])],
         device=energies.device,
         dtype=torch.bool,
     )
-
-    # Apply mask
     valid_energies = energies[valid_mask]
     valid_positions = positions[valid_mask]
 
