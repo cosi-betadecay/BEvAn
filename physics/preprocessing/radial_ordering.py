e60738aa1114a638f75d6986bdd9940a928ff776
Arya <aryaraeesi@blinkweb.no>
2026-01-12 02:48:48 -0800

    Added skeleton for radial ordering too now

---
+++ physics/preprocessing/radial_ordering.py
@@ -0,0 +1,11 @@
+import torch
+
+
+def radial_ordering(positions: torch.Tensor) -> bool:
+    return
+
+
+def radial_ordering_preprocessing(
+    energies: torch.Tensor, positions: torch.Tensor
+) -> tuple[torch.Tensor, torch.Tensor]:
+    return
