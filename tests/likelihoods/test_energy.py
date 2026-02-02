8ff721154b125fcb353506c650734e5695cd58ef
Arya <aryaraeesi@blinkweb.no>
2026-02-01 18:55:04 -0800

    24 unit tests for energy likelihood now all passing

--- tests/likelihoods/test_energy.py
+++ tests/likelihoods/test_energy.py
@@ -11,7 +11,116 @@ from mathematics.calculations import calculate_tolerance
 from physics.likelihoods.energy import energy_kernel_bdecay
 
 
-def test_energy_kernel_true_events_peak_near_511():
+# -- True Events Tests -- #
+def test_1000_2_true_events_mean():
+    gen = SyntheticDataGenerator(max_hits_per_sequence=2, seed=0)
+
+    energies, _ = gen.generate(
+        num_samples=1000,
+        mode=True,
+    )
+
+    vals = energy_kernel_bdecay(
+        energies,
+        ref_energy=511.0,
+        sigma_e=calculate_tolerance(),
+    )
+
+    assert torch.isfinite(vals).all()
+    assert torch.mean(vals) > 0.8
+
+
+def test_1000_2_true_events_max():
+    gen = SyntheticDataGenerator(max_hits_per_sequence=2, seed=0)
+
+    energies, _ = gen.generate(
+        num_samples=1000,
+        mode=True,
+    )
+
+    vals = energy_kernel_bdecay(
+        energies,
+        ref_energy=511.0,
+        sigma_e=calculate_tolerance(),
+    )
+
+    assert torch.isfinite(vals).all()
+    assert torch.max(vals) > 0.99
+
+
+def test_1000_2_true_events_min():
+    gen = SyntheticDataGenerator(max_hits_per_sequence=2, seed=0)
+
+    energies, _ = gen.generate(
+        num_samples=1000,
+        mode=True,
+    )
+
+    vals = energy_kernel_bdecay(
+        energies,
+        ref_energy=511.0,
+        sigma_e=calculate_tolerance(),
+    )
+
+    assert torch.isfinite(vals).all()
+    assert torch.min(vals) > 0.5
+
+
+def test_2000000_4_true_events_mean():
+    gen = SyntheticDataGenerator(max_hits_per_sequence=4, seed=0)
+
+    energies, _ = gen.generate(
+        num_samples=2000000,
+        mode=True,
+    )
+
+    vals = energy_kernel_bdecay(
+        energies,
+        ref_energy=511.0,
+        sigma_e=calculate_tolerance(),
+    )
+
+    assert torch.isfinite(vals).all()
+    assert torch.mean(vals) > 0.8
+
+
+def test_2000000_4_true_events_max():
+    gen = SyntheticDataGenerator(max_hits_per_sequence=4, seed=0)
+
+    energies, _ = gen.generate(
+        num_samples=2000000,
+        mode=True,
+    )
+
+    vals = energy_kernel_bdecay(
+        energies,
+        ref_energy=511.0,
+        sigma_e=calculate_tolerance(),
+    )
+
+    assert torch.isfinite(vals).all()
+    assert torch.max(vals) > 0.99
+
+
+def test_2000000_4_true_events_min():
+    gen = SyntheticDataGenerator(max_hits_per_sequence=4, seed=0)
+
+    energies, _ = gen.generate(
+        num_samples=2000000,
+        mode=True,
+    )
+
+    vals = energy_kernel_bdecay(
+        energies,
+        ref_energy=511.0,
+        sigma_e=calculate_tolerance(),
+    )
+
+    assert torch.isfinite(vals).all()
+    assert torch.min(vals) > 0.5
+
+
+def test_200_8_true_events_mean():
     gen = SyntheticDataGenerator(max_hits_per_sequence=8, seed=0)
 
     energies, _ = gen.generate(
@@ -19,7 +128,6 @@ def test_energy_kernel_true_events_peak_near_511():
         mode=True,
     )
 
-    # energies er allerede [N, max_hits]
     vals = energy_kernel_bdecay(
         energies,
         ref_energy=511.0,
@@ -27,10 +135,209 @@ def test_energy_kernel_true_events_peak_near_511():
     )
 
     assert torch.isfinite(vals).all()
-    assert vals.mean() > 0.8
+    assert torch.mean(vals) > 0.8
 
 
-def test_energy_kernel_false_events_peak_near_511():
+def test_200_8_true_events_max():
+    gen = SyntheticDataGenerator(max_hits_per_sequence=8, seed=0)
+
+    energies, _ = gen.generate(
+        num_samples=200,
+        mode=True,
+    )
+
+    vals = energy_kernel_bdecay(
+        energies,
+        ref_energy=511.0,
+        sigma_e=calculate_tolerance(),
+    )
+
+    assert torch.isfinite(vals).all()
+    assert torch.max(vals) > 0.99
+
+
+def test_200_8_true_events_min():
+    gen = SyntheticDataGenerator(max_hits_per_sequence=8, seed=0)
+
+    energies, _ = gen.generate(
+        num_samples=200,
+        mode=True,
+    )
+
+    vals = energy_kernel_bdecay(
+        energies,
+        ref_energy=511.0,
+        sigma_e=calculate_tolerance(),
+    )
+
+    assert torch.isfinite(vals).all()
+    assert torch.min(vals) > 0.5
+
+
+def test_100000_24_true_events_mean():
+    gen = SyntheticDataGenerator(max_hits_per_sequence=24, seed=0)
+
+    energies, _ = gen.generate(
+        num_samples=100000,
+        mode=True,
+    )
+
+    vals = energy_kernel_bdecay(
+        energies,
+        ref_energy=511.0,
+        sigma_e=calculate_tolerance(),
+    )
+
+    assert torch.isfinite(vals).all()
+    assert torch.mean(vals) > 0.8
+
+
+def test_100000_24_true_events_max():
+    gen = SyntheticDataGenerator(max_hits_per_sequence=24, seed=0)
+
+    energies, _ = gen.generate(
+        num_samples=100000,
+        mode=True,
+    )
+
+    vals = energy_kernel_bdecay(
+        energies,
+        ref_energy=511.0,
+        sigma_e=calculate_tolerance(),
+    )
+
+    assert torch.isfinite(vals).all()
+    assert torch.max(vals) > 0.99
+
+
+def test_100000_24_true_events_min():
+    gen = SyntheticDataGenerator(max_hits_per_sequence=24, seed=0)
+
+    energies, _ = gen.generate(
+        num_samples=100000,
+        mode=True,
+    )
+
+    vals = energy_kernel_bdecay(
+        energies,
+        ref_energy=511.0,
+        sigma_e=calculate_tolerance(),
+    )
+
+    assert torch.isfinite(vals).all()
+    assert torch.min(vals) > 0.5
+
+
+# -- False Events Tests -- #
+def test_1000_2_false_events_mean():
+    gen = SyntheticDataGenerator(max_hits_per_sequence=2, seed=0)
+
+    energies, _ = gen.generate(
+        num_samples=1000,
+        mode=False,
+    )
+
+    vals = energy_kernel_bdecay(
+        energies,
+        ref_energy=511.0,
+        sigma_e=calculate_tolerance(),
+    )
+
+    assert torch.isfinite(vals).all()
+    assert torch.mean(vals) < 0.2
+
+
+def test_1000_2_false_events_max():
+    gen = SyntheticDataGenerator(max_hits_per_sequence=2, seed=0)
+
+    energies, _ = gen.generate(
+        num_samples=1000,
+        mode=False,
+    )
+
+    vals = energy_kernel_bdecay(
+        energies,
+        ref_energy=511.0,
+        sigma_e=calculate_tolerance(),
+    )
+
+    assert torch.isfinite(vals).all()
+    assert torch.max(vals) < 0.3
+
+
+def test_1000_2_false_events_min():
+    gen = SyntheticDataGenerator(max_hits_per_sequence=2, seed=0)
+
+    energies, _ = gen.generate(
+        num_samples=1000,
+        mode=False,
+    )
+
+    vals = energy_kernel_bdecay(
+        energies,
+        ref_energy=511.0,
+        sigma_e=calculate_tolerance(),
+    )
+
+    assert torch.isfinite(vals).all()
+    assert torch.min(vals) < 0.05
+
+
+def test_2000000_4_false_events_mean():
+    gen = SyntheticDataGenerator(max_hits_per_sequence=4, seed=0)
+
+    energies, _ = gen.generate(
+        num_samples=2000000,
+        mode=False,
+    )
+
+    vals = energy_kernel_bdecay(
+        energies,
+        ref_energy=511.0,
+        sigma_e=calculate_tolerance(),
+    )
+
+    assert torch.isfinite(vals).all()
+    assert torch.mean(vals) < 0.2
+
+
+def test_2000000_4_false_events_max():
+    gen = SyntheticDataGenerator(max_hits_per_sequence=4, seed=0)
+
+    energies, _ = gen.generate(
+        num_samples=2000000,
+        mode=False,
+    )
+
+    vals = energy_kernel_bdecay(
+        energies,
+        ref_energy=511.0,
+        sigma_e=calculate_tolerance(),
+    )
+
+    assert torch.isfinite(vals).all()
+    assert torch.max(vals) < 0.3
+
+
+def test_2000000_4_false_events_min():
+    gen = SyntheticDataGenerator(max_hits_per_sequence=4, seed=0)
+
+    energies, _ = gen.generate(
+        num_samples=2000000,
+        mode=False,
+    )
+
+    vals = energy_kernel_bdecay(
+        energies,
+        ref_energy=511.0,
+        sigma_e=calculate_tolerance(),
+    )
+
+    assert torch.isfinite(vals).all()
+    assert torch.min(vals) < 0.05
+
+
+def test_200_8_false_events_mean():
     gen = SyntheticDataGenerator(max_hits_per_sequence=8, seed=0)
 
     energies, _ = gen.generate(
@@ -38,14 +345,101 @@ def test_energy_kernel_false_events_peak_near_511():
         mode=False,
     )
 
-    # energies er allerede [N, max_hits]
     vals = energy_kernel_bdecay(
         energies,
         ref_energy=511.0,
         sigma_e=calculate_tolerance(),
     )
 
-    print(vals)
+    assert torch.isfinite(vals).all()
+    assert torch.mean(vals) < 0.2
+
+
+def test_200_8_false_events_max():
+    gen = SyntheticDataGenerator(max_hits_per_sequence=8, seed=0)
+
+    energies, _ = gen.generate(
+        num_samples=200,
+        mode=False,
+    )
+
+    vals = energy_kernel_bdecay(
+        energies,
+        ref_energy=511.0,
+        sigma_e=calculate_tolerance(),
+    )
 
     assert torch.isfinite(vals).all()
-    assert vals.mean() < 0.5
+    assert torch.max(vals) < 0.3
+
+
+def test_200_8_false_events_min():
+    gen = SyntheticDataGenerator(max_hits_per_sequence=8, seed=0)
+
+    energies, _ = gen.generate(
+        num_samples=200,
+        mode=False,
+    )
+
+    vals = energy_kernel_bdecay(
+        energies,
+        ref_energy=511.0,
+        sigma_e=calculate_tolerance(),
+    )
+
+    assert torch.isfinite(vals).all()
+    assert torch.min(vals) < 0.05
+
+
+def test_100000_24_false_events_mean():
+    gen = SyntheticDataGenerator(max_hits_per_sequence=24, seed=0)
+
+    energies, _ = gen.generate(
+        num_samples=100000,
+        mode=False,
+    )
+
+    vals = energy_kernel_bdecay(
+        energies,
+        ref_energy=511.0,
+        sigma_e=calculate_tolerance(),
+    )
+
+    assert torch.isfinite(vals).all()
+    assert torch.mean(vals) < 0.2
+
+
+def test_100000_24_false_events_max():
+    gen = SyntheticDataGenerator(max_hits_per_sequence=24, seed=0)
+
+    energies, _ = gen.generate(
+        num_samples=100000,
+        mode=False,
+    )
+
+    vals = energy_kernel_bdecay(
+        energies,
+        ref_energy=511.0,
+        sigma_e=calculate_tolerance(),
+    )
+
+    assert torch.isfinite(vals).all()
+    assert torch.max(vals) < 0.3
+
+
+def test_100000_24_false_events_min():
+    gen = SyntheticDataGenerator(max_hits_per_sequence=24, seed=0)
+
+    energies, _ = gen.generate(
+        num_samples=100000,
+        mode=False,
+    )
+
+    vals = energy_kernel_bdecay(
+        energies,
+        ref_energy=511.0,
+        sigma_e=calculate_tolerance(),
+    )
+
+    assert torch.isfinite(vals).all()
+    assert torch.min(vals) < 0.05
--- tests/synthetic_data_generator.py
+++ tests/synthetic_data_generator.py
@@ -1,4 +1,4 @@
-import random
+import random as rd
 
 import numpy as np
 import torch
@@ -14,7 +14,7 @@ class SyntheticDataGenerator:
         self.max_hits = max_hits_per_sequence
         self.MEC2 = 511.0  # keV
 
-        random.seed(seed)
+        rd.seed(seed)
         torch.manual_seed(seed)
 
     def generate(
@@ -26,17 +26,22 @@ class SyntheticDataGenerator:
         positions = []
 
         for _ in range(num_samples):
-            n_hits = random.randint(2, self.max_hits)
+            n_hits = rd.randint(2, self.max_hits)
+            if mode:
+                E_remaining = self.MEC2
+            else:
+                low = rd.uniform(0.0, 490.0)
+                high = rd.uniform(530.0, 10000.0)
+                E_remaining = low if rd.random() < 0.5 else high
 
-            E_remaining = self.MEC2
             event_energies = []
             event_positions = []
 
             prev_pos = torch.zeros(3)
 
             for _ in range(n_hits - 1):
-                theta = random.uniform(0.0, np.pi / 2)
-                phi = random.uniform(0.0, 2 * np.pi)
+                theta = rd.uniform(0.0, np.pi / 2)
+                phi = rd.uniform(0.0, 2 * np.pi)
 
                 E_new = E_remaining / (1 + (E_remaining / self.MEC2) * (1 - np.cos(theta)))
                 dE = E_remaining - E_new
@@ -75,17 +80,3 @@ class SyntheticDataGenerator:
         positions = pad_sequence(positions, batch_first=True, padding_value=0.0)
 
         return energies, positions
-
-
-if __name__ == "__main__":
-    generator = SyntheticDataGenerator(max_hits_per_sequence=5, seed=42)
-    energies_false, positions_false = generator.generate(num_samples=10, mode=False)
-    energies_true, positions_true = generator.generate(num_samples=10, mode=True)
-
-    for i in range(3):
-        print(f"Sample {i + 1}:")
-        print("Energies:", energies_false[i])
-        print("Positions:", positions_false[i])
-        print("True Energies:", energies_true[i])
-        print("True Positions:", positions_true[i])
-        print()
