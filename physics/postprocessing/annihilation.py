b0b5a9ce7a52c0dade7f46c649d6a3953cc7ef0d
aryaraeesi <aryaraeesi@blinkweb.no>
2026-02-13 14:52:27 -0800

    Created 10 unit tests for annihilation kernel. All pass.

--- physics/postprocessing/annihilation.py
+++ physics/postprocessing/annihilation.py
@@ -2,15 +2,6 @@ import torch
 
 
 def annihilation_kernel(event_1_positions: torch.Tensor, event_2_positions: torch.Tensor) -> torch.Tensor:
-    """_summary_
-
-    Args:
-        event_1_positions (torch.Tensor): _description_
-        event_2_positions (torch.Tensor): _description_
-
-    Returns:
-        torch.Tensor: _description_
-    """
     pos_1 = event_1_positions if event_1_positions.ndim == 3 else event_1_positions.unsqueeze(0)
     pos_2 = event_2_positions if event_2_positions.ndim == 3 else event_2_positions.unsqueeze(0)
 
@@ -22,11 +13,15 @@ def annihilation_kernel(event_1_positions: torch.Tensor, event_2_positions: torc
     y1 = pos_2[:, 1, :]  # (B, 3)
     y0, y1 = y0[0], y1[0]
 
-    u_1 = (x1 - x0) / torch.clamp(torch.norm((x1 - x0), min=1e-3))
-    u_2 = (y1 - y0) / torch.clamp(torch.norm((y1 - y0), min=1e-3))
+    dx = x1 - x0
+    dy = y1 - y0
+
+    u_1 = dx / torch.clamp(torch.norm(dx), min=1e-3)
+    u_2 = dy / torch.clamp(torch.norm(dy), min=1e-3)
 
     cos_alpha = torch.dot(u_1, u_2)
-    mu = -1
-    sigma = 0.523598776  # 30°
+
+    mu = -1.0
+    sigma = 0.05
 
     return torch.exp(-((cos_alpha - mu) ** 2) / (2 * sigma**2))
