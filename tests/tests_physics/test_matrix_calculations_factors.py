25be878259c9948da917b2c85811e0d2e1abddcd
aryaraeesi <aryaraeesi@blinkweb.no>
2026-04-15 01:41:24 -0700

    added empty test files

--- tests/tests_physics/test_matrix_calculations_factors.py
+++ tests/tests_physics/test_matrix_calculations_factors.py
@@ -1,3 +1,4 @@
+"""
 What type of tests should be here?
 1) Tests for how matrix looks for delta_E & annihilation angle for:
     gt == True
@@ -20,3 +21,19 @@ What type of tests should be here?
     Here we should go into the .sim file, find some examples of IA ANNI's, and look at their photon output vectors (that should be 180 degreees),
     and check how close we get to that in our reconstructions.
 
+5) How much deviance there is from the annihilation algorithm reconstruction from the actual truth in average and have an assertion for that
+   so that we can make sure that the deviance isn't too big.
+
+Create plots for these tests!
+"""
+
+# Energy, etc tests
+# TODO: Test nr 1 from the top-level docstring
+# TODO: Test nr 2 from the top-level docstring
+
+# ARM
+# TODO: Test nr 3 from the top-level docstring
+
+# Annihilation Angle
+# TODO: Test nr 4 from the top-level docstring
+# TODO: Test nr 5 from the top-level docstring
